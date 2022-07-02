e4c5f451fd2f42ab39537e2839e26ba37510089f
Martin <m.bachev86@gmail.com>
2022-07-02 14:00:20 +0300

    added README.md and Practical_tasks_CRUD_Operation

--- CRUD operations/Practical_tasks_CRUD_Operation.sql
+++ CRUD operations/Practical_tasks_CRUD_Operation.sql
@@ -1,6 +1,9 @@
+-- Before run the below queries please use this file to create database and table
+-- https://github.com/martineskobg/SQL-Database-management/tree/master/Database%20management%2C%20data%20types%20and%20constraints
+
 --create customer with all fields, only with the mandatory fields
 INSERT INTO customers (name, email, phone, age, GDPR, is_profile_active, profile_deactivated, deactivation_reason, notes)
-VALUES ('Monika Beluchi','mbel@protonmail.edu','4-534-5367-8771', 70, true, false, '2022-07-01 12:10:27.128922', 'no reason',
+VALUES ('Petyr Petrov','mbel@protonmail.edu','4-534-5367-8771', 70, true, false, '2022-07-01 12:10:27.128922', 'no reason',
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum');
 
 -- Retrieving all customers
@@ -18,22 +21,22 @@ SELECT GDPR,
    FROM customers;
   
 -- Retrieving all customer info by customer id  
-SELECT * FROM customers WHERE id = 7;
+SELECT * FROM customers WHERE id = 3;
 
 -- Update customer by id
 -- Change contact phone
 -- Deactivate account
 UPDATE customers
     set phone = '0888-444-858', is_profile_active = 'false',  profile_deactivated = '2022-06-25 11:05:27.1111224'
-    WHERE id = 7;
-SELECT * FROM customers WHERE id = 7;
+    WHERE id = 3;
+SELECT * FROM customers WHERE id = 3;
 
 -- Activate account
 UPDATE customers
     set is_profile_active = 'true'
-    WHERE id = 7;
-SELECT * FROM customers WHERE id = 7;
+    WHERE id = 3;
+SELECT * FROM customers WHERE id = 3;
 
 -- Delete customer by id
-DELETE FROM customers WHERE id = 7;
+DELETE FROM customers WHERE id = 3;
 SELECT * FROM customers ORDER BY id;
---
+++ CRUD operations/README.md
@@ -0,0 +1,38 @@
+# SQL: CRUD operations
+
+### Description
+
+Practical task for all CRUD operations
+
+#### Theory:
+
+* **Create, Read, Update and Delete records**
+* **Indexes**
+* **Reading only specific columns from a table**
+* **WHERE clause basics (column=value):**
+ 
+
+#### Practical tasks:
+**Description:**
+Description: Create SQL scripts for CRUD operations of customers table:
+* create customer
+  * with all fields
+  * only with the mandatory fields
+
+* retrieving all customers
+  * query that retrieves only customer’s name and phone
+  * query that retrieves only internal information about the user - GDPR status, profile status, date created, date deactivated, reason for deactivation and the notes taken of the user 
+  * 
+* retrieving all customer info by customer id
+* update customer by id
+  * change contact phone
+  * deactivate account
+  * activate account
+* delete customer by id
+
+
+### Instruction
+Before run any of the queries in Practical_tasks_CRUD_Operation.sql, please use this file to create database and table
+[(Create database and table)]([https://github.com/martineskobg/SQL-Database-management/tree/master/Database%20management%2C%20data%20types%20and%20constraints])
+
+
